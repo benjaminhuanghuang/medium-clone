094d262b733abe91535475a7f19aca02a0357d9a
Benjamin Huang <bhuang@rms.com>
2018-06-18 11:08:34 -0700

    add utils

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/medium.css';
 import { Provider } from 'react-redux';
-
+import { Switch, Route } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import App from './App.js';
-import { store, history } from './redux/store';
-
 import registerServiceWorker from './registerServiceWorker';
+import { store, history } from './redux/store';
+import { getUser } from './redux/actions/actions'
+
+if(localStorage.Auth) {
+    // update localstorage
+    store.dispatch({type: 'SET_USER', user: JSON.parse(localStorage.Auth)})
+    var _id = JSON.parse(localStorage.Auth)._id
+    getUser(_id).then((res) => {
+        store.dispatch({type: 'SET_USER', user: res})
+    })
+}
 
 ReactDOM.render((
     <Provider store={store}>
-        <App />
+        <ConnectedRouter history={history}>
+            <Switch>
+                <Route path="/" component={App} />
+            </Switch>
+        </ConnectedRouter>
     </Provider>
 ), document.getElementById('root'));
-
 registerServiceWorker();
